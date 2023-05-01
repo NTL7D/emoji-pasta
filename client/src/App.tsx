66b702944b2a2b79264844759fb2f19e10ade227
NTL7D <nguyenthanhlong528@gmail.com>
2023-05-01 11:25:56 +0700

    add helmet title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
+import { Helmet } from "react-helmet-async";
 import "./App.css";
 import ShitPostComponent from "./shitpost";
 
 function App() {
     return (
         <div className='App'>
+            <Helmet>
+                <title>
+                    Emoji Pasta siêu ỉa chảy
+                </title>
+            </Helmet>
             <ShitPostComponent />
         </div>
     );
