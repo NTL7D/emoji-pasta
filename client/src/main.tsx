e27bc76a951712ef4341327ca8fd09a0f71c6688
NTL7D <nguyenthanhlong528@gmail.com>
2023-05-01 11:56:43 +0700

    vercel analyst

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { HelmetProvider as Header } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <Header>
             <App />
+            <Analytics />
         </Header>
     </React.StrictMode>
 );
